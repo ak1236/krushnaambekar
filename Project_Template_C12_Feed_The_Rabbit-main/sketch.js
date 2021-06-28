62350eca6ebe713947520a16fe2df3bf5fda87e8
ak1236 <86178767+ak1236@users.noreply.github.com>
2021-06-27 23:56:39 -0700

    Add files via upload

---
+++ Project_Template_C12_Feed_The_Rabbit-main/sketch.js
@@ -0,0 +1,39 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
+
+function draw() {
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+}
+
+function createApples()
+{
+  apple = createSprite(random(50,350),40,10,10);
+
+
+  apple.velocityY = 2;
+}
